3bae19941d1b272d08a95db0b6dd7dcc6073f943
conanak99 <huyhoang8a5@gmail.com>
2015-07-09 00:05:05 +0700

    Finish menu and refactor code

--- LDBChallenge/js/app/app.js
+++ LDBChallenge/js/app/app.js
@@ -1,1 +1,20 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngAnimate', 'ngAudio', 'ngRoute']);
+
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.
+    when('/menu', {
+        templateUrl: 'menu.html',
+        controller: 'menuCtrl'
+    }).
+    when('/game', {
+        templateUrl: 'game.html',
+        controller: 'gameCtrl'
+    }).
+    when('/leaderboard', {
+        templateUrl: 'leaderboard.html',
+        controller: 'leaderboardCtrl'
+    }).
+    otherwise({
+        redirectTo: '/menu'
+    });
+}]);
