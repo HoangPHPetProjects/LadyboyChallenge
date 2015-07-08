976273c00bc86022b64e0e4bd4b2650154040671
conanak99 <huyhoang8a5@gmail.com>
2015-07-09 00:11:18 +0700

    [H.Pham] Fix all bug, release version 1.0

--- js/app/controllers.js
+++ js/app/controllers.js
@@ -1,102 +1,112 @@
 var app = angular.module('app');
 
-app.controller('fbController', ['$scope', '$q', 'fbService', 'notifyService', 'storageService',
-        function ($scope, $q, fbService, notifyService, storageService) {
+app.controller('gameCtrl', ['$scope', '$http', 'ngAudio', function ($scope, $http, ngAudio) {
+    $scope.point = 0;
+    $scope.showImg = false;
+    $scope.playerName = '';
 
-            $scope.userStatus = '';
-            $scope.message = '';
-            $scope.link = '';
-            $scope.groupId = '';
-            $scope.userId = '';
-            $scope.savedGroups = [];
+    $scope.choices = [];
+    $scope.leaderboard = [];
 
-            fbService.init()
-                .then(function (response) { Materialize.toast(response, 2000); })
-                .then(fbService.getStatus)
-                .then(function (response) {
-                    NProgress.start();
-                    $scope.userId = response.id;
-                    $scope.userStatus = 'Logging in as: ' + response.name + '!';
-                    return response.id;
-                })
-                .then(storageService.getSavedGroups)
-                .then(function (result) {
-                    $scope.savedGroups = result;
-                    notifyService.shortNotify(result.length + ' groups loaded.');
-                    NProgress.done();
-                });
+    var bingoSound = ngAudio.load("https://raw.githubusercontent.com/HoangPHPetProjects/LadyboyChallenge/gh-pages/sound/right.wav");
+    var failSound = ngAudio.load("https://raw.githubusercontent.com/HoangPHPetProjects/LadyboyChallenge/gh-pages/sound/wrong.wav");
+    failSound.volume = 0.3;
 
-            $scope.login = function () {
-                fbService.login()
-                    .then(function (response) {
-                        NProgress.start();
-                        $scope.userId = response.id;
-                        $scope.userStatus = 'Logging in as: ' + response.name + '!';
-                        return response.id;
-                    })
-                    .then(storageService.getSavedGroups)
-                    .then(function (result) {
-                        $scope.savedGroups = result;
-                        notifyService.shortNotify(result.length + ' groups loaded.');
-                        NProgress.done();
-                });;
-            };
+    var girls = [];
+    var ladyboys = [];
 
-            $scope.postMessage = function () {
-                NProgress.start();
+    loadResources();
 
-                var promises = $scope.savedGroups
-                    .filter(function (gr) { return gr.checked; })
-                    .map(function (gr) { return gr.id; })
-                    .map(function (id) {
-                        return fbService.postMessage(id, $scope.message, $scope.link).then(function (response) {
-                            notifyService.shortNotify('Posted successful with id: ' + response.id + '.');
-                            return response;
-                        });
-                    });
+    $scope.choiceClick = function (item) {
+        $scope.showImg = false;
+        if (item.isGirl) {
+            bingoSound.play();
+            $scope.point++;
+            $('#rightModal').openModal({
+                complete: function () {
+                    $scope.showImg = true;
+                    $scope.$digest();
+                }
+            });
+            loadImage($scope.point);
+        } else {
+            failSound.play();
+            $('#wrongModal').openModal({
+                complete: function () {
+                    $scope.point = 0;
+                    $scope.showImg = true;
+                    $scope.$digest();
+                }
+            });
+            loadImage(0);
+        }
+    };
 
-                $q.all(promises).then(function (values) {
-                    notifyService.longNotify('Posted in ' + values.length + ' groups.');
-                    $scope.message = '';
-                    $scope.link = '';
-                    NProgress.done();
-                });
-            };
+    $scope.sendScore = function () {
+        var scoreUrl = 'https://api.mongolab.com/api/1/databases/ladyboy_challenge/collections/leaderBoard?apiKey=Rds2DpkLY7_VqsMfmgfSo_EdzafbQvOs';
+        var entry = { name: $scope.playerName, score: $scope.point };
 
-            $scope.addGroup = function () {
-                NProgress.start();
-                fbService.getGroupInfo($scope.groupId)
-                    .then(function (response) {
-                        NProgress.done();
-                        var foundGroup = $scope.savedGroups.filter(function (gr) {
-                            return gr.id == $scope.groupId;
-                        })[0];
-                        if (foundGroup != null) {
-                            notifyService.shortNotify('Group "' + response.name + '" is already added.');
-                            return;
-                        };
+        $http.post(scoreUrl, entry).then(function () {
+            Materialize.toast('Your score has been submitted.', 3000);
+            $('#wrongModal').closeModal();
+            $scope.point = 0;
+            $scope.showImg = true;
+            $scope.playerName = '';
+        });
 
-                        notifyService.longNotify('Group "' + response.name + '" added.');
-                        $scope.groupId = '';
-                        $scope.savedGroups.push(response);
-                    });
-            };
+    }
 
-            $scope.deleteGroup = function (group) {
-                $scope.savedGroups = $scope.savedGroups.filter(function (gr) {
-                    return gr.id != group.id;
-                });
-            }
+    function loadImage(point) {
+        var items = [];
+        items.push(getRandomItem(girls));
 
-            $scope.saveGroups = function () {
-                //localStorage.setItem("fbPost_savedGroup", JSON.stringify($scope.savedGroups));
-                NProgress.start();
-                storageService.saveGroups($scope.userId, $scope.savedGroups)
-                    .then(function(result) {
-                        NProgress.done();
-                        notifyService.longNotify($scope.savedGroups.length + " groups saved.");
-                    });
-                
-            }
+        if (point < 10) {
+            items.push(getRandomItem(ladyboys));
+        } else {
+            items.push(getRandomItem(ladyboys));
+            items.push(getRandomItem(ladyboys));
+            items.push(getRandomItem(ladyboys));
+        };
 
-        }]);
+        if (point == 10) Materialize.toast('Things are getting harder. Be careful!!', 4000);
+        $scope.choices = shuffleArray(items);
+    };
+
+    function loadResources() {
+        var girlUrl = 'https://api.mongolab.com/api/1/databases/ladyboy_challenge/collections/girls?apiKey=Rds2DpkLY7_VqsMfmgfSo_EdzafbQvOs';
+        var ldbUrl = 'https://api.mongolab.com/api/1/databases/ladyboy_challenge/collections/ladyboys?apiKey=Rds2DpkLY7_VqsMfmgfSo_EdzafbQvOs';
+
+        $http.get(girlUrl).then(function (rs) {
+            girls = rs.data;
+            return ldbUrl;
+        }).then($http.get).then(function (rs) {
+            ladyboys = rs.data;
+            return $scope.point;
+        }).then(function () {
+            loadImage($scope.point);
+            $scope.showImg = true;
+        });
+    };
+}
+]);
+
+
+app.controller('leaderboardCtrl', ['$scope', '$http', function ($scope, $http) {
+    $scope.leaderboard = [];
+
+    loadLeaderboard();
+
+    function loadLeaderboard() {
+        //Sort by score, only get top ten
+        var scoreUrl = 'https://api.mongolab.com/api/1/databases/ladyboy_challenge/collections/' +
+                       'leaderBoard?s={"score":-1}&l=10&apiKey=Rds2DpkLY7_VqsMfmgfSo_EdzafbQvOs';
+
+        $http.get(scoreUrl).then(function (rs) {
+            $scope.leaderboard = rs.data;
+        });
+    };
+}]);
+
+app.controller('menuCtrl', ['$scope', function ($scope) {
+    //Nothing here
+}]);
